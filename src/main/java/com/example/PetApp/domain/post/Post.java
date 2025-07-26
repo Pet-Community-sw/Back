116ba80fdeff81f4eb485907cc68945910bf2dc4
sunjaewow <chltjswo789@naver.com>
2025-07-26 12:34:00 +0900

    수정

--- src/main/java/com/example/PetApp/domain/post/Post.java
+++ src/main/java/com/example/PetApp/domain/post/Post.java
@@ -2,17 +2,19 @@ package com.example.PetApp.domain.post;
 
 import com.example.PetApp.domain.Member;
 import com.example.PetApp.domain.embedded.PostContent;
+import com.example.PetApp.domain.superclass.BaseTimeEntity;
 import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @DiscriminatorColumn(name = "post_type")
 @Builder
 @Getter
-public abstract class Post {
+public abstract class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
