5d56f53439a298ccdfb5eeabe691dfd9faf05bdd
sunjaewow <chltjswo789@naver.com>
2025-07-26 12:47:33 +0900

    single 전략

--- src/main/java/com/example/PetApp/domain/like/Like.java
+++ src/main/java/com/example/PetApp/domain/like/Like.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "like_type")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
