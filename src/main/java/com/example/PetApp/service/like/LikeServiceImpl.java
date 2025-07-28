b7f02dffc0e18b508863e1d6807a2e0bbd8e092f
sunjaewow <chltjswo789@naver.com>
2025-07-28 16:13:46 +0900

    likeService refactoring

--- src/main/java/com/example/PetApp/service/like/LikeServiceImpl.java
+++ src/main/java/com/example/PetApp/service/like/LikeServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.PetApp.service.like;
 import com.example.PetApp.domain.Member;
 import com.example.PetApp.domain.like.Like;
 import com.example.PetApp.domain.post.Post;
-import com.example.PetApp.domain.RecommendRoutePost;
 import com.example.PetApp.dto.like.LikeResponseDto;
 import com.example.PetApp.exception.NotFoundException;
 import com.example.PetApp.mapper.LikeMapper;
@@ -25,7 +24,6 @@ import java.util.Optional;
 public class LikeServiceImpl implements LikeService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
-    private final RecommendRoutePostRepository recommendRoutePostRepository;
     private final LikeRepository likeRepository;
     private final SendNotificationUtil sendNotificationUtil;
 
@@ -39,7 +37,9 @@ public class LikeServiceImpl implements LikeService {
         return LikeMapper.toLikeResponseDto(post.getLikes());
     }
 
-    public ResponseEntity<?> createAndDeleteLike(Long postId, String email) {
+    @Transactional
+    @Override
+    public ResponseEntity<String> createAndDeleteLike(Long postId, String email) {
         log.info("createAndDeleteLike 요청 postId : {}", postId);
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new NotFoundException("회원이 존재하지 않습니다."));
@@ -48,95 +48,32 @@ public class LikeServiceImpl implements LikeService {
         Optional<Like> existingLike = post.getLikes().stream()
                 .filter(like -> like.getMember().equals(member))
                 .findFirst();
-        if (existingLike.isPresent()) {
-            Like like = existingLike.get();
-            likeRepository.delete(like);
-        } else {
-
-        }
-
-
+        return existingLike
+                .map(this::deleteLike)
+                .orElseGet(() -> createLike(post, member));
     }
 
-    @Transactional
-    @Override
-    public ResponseEntity<?> createAndDeleteLike(Long postId, String email) {
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundException("회원이 존재하지 않습니다."));
-
-        Object post;
-        switch (postId.getPostType()) {
-            case COMMUNITY -> {
-                post = postRepository.findById(postId.getPostId())
-                        .orElseThrow(() -> new NotFoundException("해당 게시물은 없습니다."));
-            }
-            case RECOMMEND -> {
-                post = recommendRoutePostRepository.findById(postId.getPostId())
-                        .orElseThrow(() -> new NotFoundException("해당 산책길 추천 게시글은 없습니다."));
-            }
-            default -> {
-                throw new IllegalArgumentException("잘못된 postType입니다.");
-            }
-        }
-
-        // 좋아요 존재 여부
-        boolean isLiked = existsLike(post, member);
-
-        if (isLiked) {
-            log.info("좋아요 삭제");
-            deleteLike(post, member);
-            return ResponseEntity.ok().body("좋아요 삭제했습니다.");
-        } else {
-            log.info("좋아요 생성");
-            sendLikeNotification(post, member);
-            createLike(post, member);
-            return ResponseEntity.status(HttpStatus.CREATED).body("좋아요 생성했습니다.");
-        }
+    private ResponseEntity<String> deleteLike(Like like) {
+        log.info("좋아요 삭제");
+        likeRepository.delete(like);
+        return ResponseEntity.ok("좋아요 삭제했습니다.");
     }
 
-    private <T> void createLike(T postEntity, Member member) {
-        LikeT.LikeTBuilder builder = LikeT.builder().member(member);
-        if (postEntity instanceof Post) {
-            builder.post((Post) postEntity);
-        } else if (postEntity instanceof RecommendRoutePost) {
-            builder.recommendRoutePost((RecommendRoutePost) postEntity);
-        } else {
-            throw new IllegalArgumentException("지원하지 않는 타입입니다.");
-        }
-        likeRepository.save(builder.build());
+    private ResponseEntity<String> createLike(Post post, Member member) {
+        log.info("좋아요 생성");
+        Like like = post.createLike(member);
+        post.getLikes().add(like);
+        likeRepository.save(like);
+
+        sendNotification(post, member);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("좋아요 생성했습니다.");
+    }
+
+    private void sendNotification(Post post, Member member) {
+        String message = member.getName() + "님이 회원님의 게시물을 좋아합니다.";
+        sendNotificationUtil.sendNotification(post.getMember(), message);
     }
 
 
-    private <T> void deleteLike(T postEntity, Member member) {
-        if (postEntity instanceof Post) {
-            likeRepository.deleteByPostAndMember((Post) postEntity, member);
-        } else if (postEntity instanceof RecommendRoutePost) {
-            likeRepository.deleteByRecommendRoutePostAndMember((RecommendRoutePost) postEntity, member);
-        } else {
-            throw new IllegalArgumentException("지원하지 않는 타입입니다.");
-        }
-    }
-
-    private <T> boolean existsLike(T postEntity, Member member) {
-        if (postEntity instanceof Post) {
-            return likeRepository.existsByPostAndMember((Post) postEntity, member);
-        } else if (postEntity instanceof RecommendRoutePost) {
-            return likeRepository.existsByRecommendRoutePostAndMember((RecommendRoutePost) postEntity, member);
-        } else {
-            throw new IllegalArgumentException("지원하지 않는 타입입니다.");
-        }
-    }
-
-    private <T> void sendLikeNotification(T postEntity, Member liker) {
-        Member postOwner;
-        if (postEntity instanceof Post) {
-            postOwner = ((Post) postEntity).getMember();
-        } else if (postEntity instanceof RecommendRoutePost) {
-            postOwner = ((RecommendRoutePost) postEntity).getMember();
-        } else {
-            throw new IllegalArgumentException("지원하지 않는 타입입니다.");
-        }
-        String message = liker.getName() + "님이 회원님의 게시물을 좋아합니다.";
-            sendNotificationUtil.sendNotification(postOwner, message);
-    }
 }
